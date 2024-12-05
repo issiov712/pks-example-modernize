8479a4ac498daebf56829374a58d3ec67f1b5ec5
Peter Shiner <peter.shiner@jumpycatsystems.com>
2024-12-04 20:33:56 -0500

    messing w/ a custom logback appender; this would be used to push logging events into the database in the format desired by the customer;

---
+++ app-domain/src/main/java/pks/example/modernize/util/CustomLogBackAppender.java
@@ -0,0 +1,31 @@
+package pks.example.modernize.util;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+// import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.ConsoleAppender;
+import lombok.Getter;
+import lombok.Setter;
+
+
+/**
+ * <p>This CustomLogBackAppender works, for what it is.  The better way to rewrite the message is to use a custom encoder.
+ * This shows how we can just write a normal appender and grab logging event and then spool them into a database in a particular 
+ * format.  There is also an existing database appender that uses three (3) tables for the types of events. ( See: 
+ * <a href="https://logback.qos.ch/manual/appenders.html#DBAppender">https://logback.qos.ch/manual/appenders.html#DBAppender</a> )</p>
+ * 
+ * <p>This is the correct way to intercept the logging event and send it to a different logging sink using the logging event as already defined.</p>
+ */
+@Getter @Setter
+public class CustomLogBackAppender extends ConsoleAppender<ILoggingEvent> {
+
+    private String prefix;
+
+    /*
+     * Way too much moving bytes around in formatting to be quick.
+     */
+    @Override
+    protected void append(final ILoggingEvent event) {
+        System.out.print(prefix + " " + new String(super.encoder.encode(event)));
+    }
+
+}
