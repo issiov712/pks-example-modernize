f44962d639514fb009a2395259cf0a21a7ef72db
Peter Shiner <peter.shiner@jumpycatsystems.com>
2024-12-15 16:27:33 -0500

    works: added real loggin via slf4j and logback; need to use older logback for java 8; but works java 8, 11, 17, 21;

--- maven-lombok-with-aspectj-master/src/main/java/com/test/SetterAspect.java
+++ maven-lombok-with-aspectj-master/src/main/java/com/test/SetterAspect.java
@@ -1,6 +1,9 @@
 package com.test;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.Locale;
+import javax.money.format.AmountFormatQueryBuilder;
+
 import javax.money.format.MonetaryAmountFormat;
 import javax.money.format.MonetaryFormats;
 
@@ -11,17 +14,44 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
 import org.javamoney.moneta.Money;
+import org.javamoney.moneta.format.CurrencyStyle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Aspect
 public class SetterAspect {
-	MonetaryAmountFormat formatUSD = MonetaryFormats.getAmountFormat(Locale.US);
+
+	// How-to: https://peterdev.pl/2020/02/07/formatting-monetary-amounts-with-java-money/
+	MonetaryAmountFormat formatUSD = MonetaryFormats.getAmountFormat(
+		AmountFormatQueryBuilder.of(Locale.US)
+			.set(CurrencyStyle.SYMBOL)
+			.build());
+
+	ConcurrentHashMap<String,Logger> clazzLoggers = new ConcurrentHashMap<>();
+
+	Logger getJoinPointLogger(ProceedingJoinPoint joinPoint) {
+		String clazzName = joinPoint.getSignature().getDeclaringTypeName();
+		//String clazzName = joinPoint.getTarget().getClass().getName();
+		Logger logger = clazzLoggers.get(clazzName);
+		if (logger == null) { 
+			logger = LoggerFactory.getLogger(clazzName);
+			clazzLoggers.put(clazzName,logger);
+		}
+		return logger;
+	}
+
 
     @Around(value = "execution(* com.test.entity.*.set*(..))")
     public void checkBeforeCallingSetter(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		Logger log = getJoinPointLogger(joinPoint);
+
         Object valueToBeSet = joinPoint.getArgs()[0];
 
         if (valueToBeSet != null && isNotEmpty(valueToBeSet)) {
-            System.out.println("proceeding");
+            log.info("proceeding");
             joinPoint.proceed();
         }
         
@@ -38,17 +68,26 @@ public class SetterAspect {
 
 	@Around(value = "execution(* com.test..build(..))")
 	public Object checkXferObjects(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		Logger log = getJoinPointLogger(joinPoint);
+
 		Object obj = joinPoint.proceed();
-		System.out.println("build: " + obj.getClass());
+		log.info("build: " + obj.getClass());
+
 		return obj;
 	}
 
 	@Around(value = "set(org.javamoney.moneta.Money com.test.entity..*)")
 	public void checkMoney(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		Logger log = getJoinPointLogger(joinPoint);
+
 		Money m = (Money)joinPoint.getArgs()[0];
-		System.out.print("thieving a money slice off the top from '" + formatUSD.format(m));
+		String msg = "thieving a money slice off the top from '" + formatUSD.format(m);
 		m = m.multiply(0.99d);
-		System.out.println("' to '" + formatUSD.format(m) + "'");
+		msg = msg + "' to '" + formatUSD.format(m) + "'";
+		log.warn(msg);
+
 		Object[] r = { (Object)m };
 		joinPoint.proceed(r);
 	}
