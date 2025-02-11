a47aecd29dbea2d88cc113c13386c6bb7ed63ee1
Felicia Imbriano <fimbriano@metaphaseconsulting.com>
2025-02-11 10:05:18 -0500

    feat: add CreateLoan component and integrate with LoanTerms page for loan creation

--- quick-loan-ui/src/pages/LoanTerms.js
+++ quick-loan-ui/src/pages/LoanTerms.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 import ProgramCommitmentTable from '../components/ProgramCommitment/ProgramCommitmentTable';
 import Container from '../components/layout/Container';
+import { Box, Button } from '@mui/material';
+import CreateLoan from '../components/CreateLoan';
 
 export default function LoanTerms() {
-  return (
-    <Container>
-      <ProgramCommitmentTable />
-    </Container>
-  );
+    const [showModal, setShowModal] = useState(false);
+    
+    const handleShowModal = () => {
+        setShowModal(!showModal);
+    }
+
+    return (
+        <Container>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <Button variant="contained" color="primary" onClick={handleShowModal}>Add Loan</Button>
+            </Box>
+            <ProgramCommitmentTable />
+            <CreateLoan showModal={showModal} handleShowModal={handleShowModal} />
+        </Container>
+    );
 }
 
 
