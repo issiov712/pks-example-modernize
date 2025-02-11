fe6cccd1395948422141286f41f3fed7d615057d
Peter Shiner <peter.shiner@jumpycatsystems.com>
2025-02-10 19:25:04 -0500

    trying using kendo;

--- quick-loan/src/main/java/pks/example/quick/infrastructure/rest/LoanController.java
+++ quick-loan/src/main/java/pks/example/quick/infrastructure/rest/LoanController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/loan")
 public class LoanController {
---
+++ quick-ui-pks/quick-ui-pks/src/gd-interfaces.ts
@@ -0,0 +1,12 @@
+export interface LoanType {
+	Id: string;
+	Name: string;
+	Description: string;
+}
+
+export interface PaymentType {
+	Date: string;
+	Payment: string;
+	Interest: string;
+	Principal: string;
+}
